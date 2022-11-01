37f79f26d93673f57ba09a8359f6980d56f9fae7
dimkauzh <106883655+dimkauzh@users.noreply.github.com>
2022-11-01 11:22:29 +0100

    Update maze2.py

--- Maze/2/maze2.py
+++ Maze/2/maze2.py
@@ -9,56 +9,80 @@ level1 = pew.Pix.from_iter((
     [1, 0, 0, 0, 0, 1, 0, 1],
     [1, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1],
 ))
 
 level2 = pew.Pix.from_iter((
     [1, 1, 1, 1, 1, 2, 0, 2],
+    [1, 1, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 0, 2, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 1, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 2, 0, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1],
 ))
 
 level3 = pew.Pix.from_iter((
     [1, 1, 1, 1, 1, 2, 0, 2],
     [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1],
     [1, 1, 1, 1, 1, 2, 0, 2],
 ))
 
 x = 6
 y = 0
+stand_x = 6
+stand_y = 0
 
-level = level2
+level = level3
 
-def clear():
-    level.pixel(x, y, 0)
+def clear(x, y, num):
+    level.pixel(x, y, num)
 while True:
     # Level 1
     if level == level1:
         # Next level
         if x == 1 and y == 2:
-            clear()
+            clear(1, 2, 0)
             level = level2
             x = 6
             y = 0
-            clear()
     # Level 2
     if level == level2:
+        if x == 1 and y == 2:
+            level.pixel(6, 3, 0)
+            x = stand_x
+            y = stand_y
+        #Next Level
+        if x == 1 and y == 4:
+            level = level3
+            clear(1, 4, 0)
         # Previous level
         if x == 6 and y == -1:
             level = level1
             x = 6
             y = 0
- 
+            clear(6, -1, 0)
+    # Level 3       
+    if level == level3:
+        #Teleport
+        if x == 4 and y == 6:
+            x = 6
+            y = 3
+            clear(4, 6, 0)
+        #Previous level
+        if x == 6 and y == -1:
+            level = level2
+            x = 6
+            y = 0
+            clear(6, -1, 0)
+        
+
     level.pixel(x, y, 0)
     keys = pew.keys()
     dx = 0
